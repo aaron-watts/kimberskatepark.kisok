31285edf37d7317a309ee6c2535f31b6ba140eff
Aaron Watts <aaron.watts@murena.io>
2025-02-11 10:15:16 +0000

    move all current files to git repo

---
+++ scripts/run-feh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+feh /home/display/Pictures/ --fullscreen --slideshow-delay 20 
---
+++ scripts/run_display.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+/home/display/scripts/run-chromium.sh &
+sleep 1.5
+/home/display/scripts/run-feh.sh &
